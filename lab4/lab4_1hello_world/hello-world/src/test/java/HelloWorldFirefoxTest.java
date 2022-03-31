48afb769e6e01a574dd07a1b2e10ca9a3a0b50c4
Daniel Francisco <deny.jo@hotmail.com>
2022-03-31 17:55:32 +0100

    Complete exercise 1

---
+++ lab4/lab4_1hello_world/hello-world/src/test/java/HelloWorldFirefoxTest.java
@@ -0,0 +1,46 @@
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HelloWorldFirefoxTest {
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new FirefoxDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        // Exercise
+        String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
+        driver.get(sutUrl);
+        String title = driver.getTitle();
+        log.debug("The title of {} is {}", sutUrl, title);
+
+        // Verify
+        assertThat(title, equalTo("Hands-On Selenium WebDriver with Java"));
+    }
+}

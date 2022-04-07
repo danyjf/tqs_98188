3a0ab1fc16078ddd0195b488460e18680323d312
Daniel Francisco <deny.jo@hotmail.com>
2022-04-07 17:09:29 +0100

    Complete lab 5 exercise 1 calculator example

--- lab5/lab5_1getting_started/cucumber-java-skeleton/src/main/java/io/cucumber/skeleton/Belly.java
+++ lab5/lab5_1getting_started/cucumber-java-skeleton/src/main/java/io/cucumber/skeleton/Belly.java
@@ -2,6 +2,14 @@ package io.cucumber.skeleton;
 
 public class Belly {
     public void eat(int cukes) {
+        System.out.println("Eating " + cukes + " cukes.");
+    }
 
+    public void wait(int hours) {
+        System.out.println("Wait " + hours + " hours.");
+    }
+
+    public void growl() {
+        System.out.println("Belly growl");
     }
 }
--- lab5/lab5_1getting_started/cucumber-java-skeleton/src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ lab5/lab5_1getting_started/cucumber-java-skeleton/src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -5,21 +5,20 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefinitions {
+    Belly belly = new Belly();
+
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
-        Belly belly = new Belly();
         belly.eat(cukes);
     }
 
     @When("I wait {int} hour")
-    public void i_wait_hour(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println("Wait " + int1 + " hour");
+    public void i_wait_hour(Integer hours) {
+        belly.wait(hours);
     }
 
     @Then("my belly should growl")
     public void my_belly_should_growl() {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println("Belly growl");
+        belly.growl();
     }
 }

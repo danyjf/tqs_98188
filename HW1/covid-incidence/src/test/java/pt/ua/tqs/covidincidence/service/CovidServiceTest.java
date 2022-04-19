735d3bccb4551ced0c0cf3485712981340e2099f
Daniel Francisco <deny.jo@hotmail.com>
2022-04-19 14:02:39 +0100

    Fix service test

--- HW1/covid-incidence/src/test/java/pt/ua/tqs/covidincidence/service/CovidServiceTest.java
+++ HW1/covid-incidence/src/test/java/pt/ua/tqs/covidincidence/service/CovidServiceTest.java
@@ -3,6 +3,7 @@ package pt.ua.tqs.covidincidence.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -31,32 +32,15 @@ public class CovidServiceTest {
     public CovidServiceTest() {
     }
 
-    @BeforeEach
-    public void setUp() {
+    @Test
+    public void whenGetCovidStatsForPortugalOnDate_thenCorrectCovidStatsShouldBeReturned() {
         Mockito.when(webClientMock.get()).thenReturn(uriSpecMock);
-        Mockito.when(uriSpecMock.uri("https://covid-193.p.rapidapi.com/history?country=Portugal&day=2021-06-09")).thenReturn(headersSpecMock);
+        Mockito.when(uriSpecMock.uri(ArgumentMatchers.<String>notNull())).thenReturn(headersSpecMock);
         Mockito.when(headersSpecMock.retrieve()).thenReturn(responseSpecMock);
         Mockito.when(responseSpecMock.bodyToMono(String.class)).thenReturn(Mono.just(
                 "{\"get\":\"history\",\"parameters\":{\"country\":\"Portugal\",\"day\":\"2021-06-09\"},\"errors\":[],\"results\":3,\"response\":[{\"continent\":\"Europe\",\"country\":\"Portugal\",\"population\":10168845,\"cases\":{\"new\":\"+890\",\"active\":23996,\"critical\":70,\"recovered\":813489,\"1M_pop\":\"84033\",\"total\":854522},\"deaths\":{\"new\":null,\"1M_pop\":\"1675\",\"total\":17037},\"tests\":{\"1M_pop\":\"1188905\",\"total\":12089787},\"day\":\"2021-06-09\",\"time\":\"2021-06-09T22:00:02+00:00\"},{\"continent\":\"Europe\",\"country\":\"Portugal\",\"population\":10168845,\"cases\":{\"new\":\"+598\",\"active\":23631,\"critical\":66,\"recovered\":812964,\"1M_pop\":\"83946\",\"total\":853632},\"deaths\":{\"new\":\"+1\",\"1M_pop\":\"1675\",\"total\":17037},\"tests\":{\"1M_pop\":\"1188905\",\"total\":12089787},\"day\":\"2021-06-09\",\"time\":\"2021-06-09T15:15:03+00:00\"},{\"continent\":\"Europe\",\"country\":\"Portugal\",\"population\":10168925,\"cases\":{\"new\":\"+598\",\"active\":23631,\"critical\":66,\"recovered\":812964,\"1M_pop\":\"83945\",\"total\":853632},\"deaths\":{\"new\":\"+1\",\"1M_pop\":\"1675\",\"total\":17037},\"tests\":{\"1M_pop\":\"1188895\",\"total\":12089787},\"day\":\"2021-06-09\",\"time\":\"2021-06-09T00:00:02+00:00\"}]}"
         ));
 
-        Mockito.when(webClientMock.get()).thenReturn(uriSpecMock);
-        Mockito.when(uriSpecMock.uri("https://covid-193.p.rapidapi.com/history?country=Wrong Country&day=2021-06-09")).thenReturn(headersSpecMock);
-        Mockito.when(headersSpecMock.retrieve()).thenReturn(responseSpecMock);
-        Mockito.when(responseSpecMock.bodyToMono(String.class)).thenReturn(Mono.just(
-                "{\"get\":\"history\",\"parameters\":{\"country\":\"Wrong Country\",\"day\":\"2021-06-09\"},\"errors\":[],\"results\":0,\"response\":[]}"
-        ));
-
-        Mockito.when(webClientMock.get()).thenReturn(uriSpecMock);
-        Mockito.when(uriSpecMock.uri("https://covid-193.p.rapidapi.com/history?country=Portugal&day=2010-01-01")).thenReturn(headersSpecMock);
-        Mockito.when(headersSpecMock.retrieve()).thenReturn(responseSpecMock);
-        Mockito.when(responseSpecMock.bodyToMono(String.class)).thenReturn(Mono.just(
-                "{\"get\":\"history\",\"parameters\":{\"country\":\"Wrong Country\",\"day\":\"2021-06-09\"},\"errors\":[],\"results\":0,\"response\":[]}"
-        ));
-    }
-
-    @Test
-    public void whenGetCovidStatsForPortugalOnDate_thenCorrectCovidStatsShouldBeReturned() {
         CovidStats covidStats = covidService.getCovidStatsByCountryAndDate("Portugal", "2021-06-09");
 
         CovidStats expectedStats = new CovidStats(
@@ -81,6 +65,13 @@ public class CovidServiceTest {
 
     @Test
     public void whenGetCovidStatsForNonExistingCountry_thenCovidStatsShouldBeNull() {
+        Mockito.when(webClientMock.get()).thenReturn(uriSpecMock);
+        Mockito.when(uriSpecMock.uri(ArgumentMatchers.<String>notNull())).thenReturn(headersSpecMock);
+        Mockito.when(headersSpecMock.retrieve()).thenReturn(responseSpecMock);
+        Mockito.when(responseSpecMock.bodyToMono(String.class)).thenReturn(Mono.just(
+                "{\"get\":\"history\",\"parameters\":{\"country\":\"Wrong Country\",\"day\":\"2021-06-09\"},\"errors\":[],\"results\":0,\"response\":[]}"
+        ));
+
         CovidStats covidStats = covidService.getCovidStatsByCountryAndDate("Wrong Country", "2021-06-09");
 
         assertThat(covidStats).isNull();
@@ -88,6 +79,13 @@ public class CovidServiceTest {
 
     @Test
     public void whenGetCovidStatsForDateWithNoData_thenCovidStatsShouldBeNull() {
+        Mockito.when(webClientMock.get()).thenReturn(uriSpecMock);
+        Mockito.when(uriSpecMock.uri(ArgumentMatchers.<String>notNull())).thenReturn(headersSpecMock);
+        Mockito.when(headersSpecMock.retrieve()).thenReturn(responseSpecMock);
+        Mockito.when(responseSpecMock.bodyToMono(String.class)).thenReturn(Mono.just(
+                "{\"get\":\"history\",\"parameters\":{\"country\":\"Portugal\",\"day\":\"2010-01-01\"},\"errors\":[],\"results\":0,\"response\":[]}"
+        ));
+
         CovidStats covidStats = covidService.getCovidStatsByCountryAndDate("Portugal", "2010-01-01");
 
         assertThat(covidStats).isNull();
